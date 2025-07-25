7230cd377ad1180aebfa6f7e7d2a4c8ff1fe8cf4
River Route Creative Group <riverroutecg@gmail.com>
2025-07-25 15:12:47 +0530

    25 july chnages

--- src/Add.jsx
+++ src/Add.jsx
@@ -1,17 +1,108 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Add() {
   return (
-    <div className='addButtons'>
-      <div className='addBudget'>
-            <img src='../src/assets/icons/plus-icon.png' />
-            <p>Add Budget</p>
+    <>
+      <div className='addButtons'>
+        
+
+      <button className="addBudget" data-bs-toggle="modal" data-bs-target="#addBudgetModal">
+          <img src="../src/assets/icons/plus-icon.png" alt="Add Budget" />
+        Add Budget
+      </button>
+
+        <button className='addExpense' data-bs-toggle="modal" data-bs-target="#addExpenseModal">
+          <img src="../src/assets/icons/plus-icon.png" alt="Add Expense" />
+          <p>Add Expense</p>
+        </button>
       </div>
 
-      <div className='addExpense'>
-            <img src='../src/assets/icons/plus-icon.png' />
-            <p>Add Expense</p>
-      </div>
-    </div>
-  )
+
+
+
+      
+
+      <div className="modal fade" id="addBudgetModal" tabIndex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+
+            <div className="modal-header">
+              <h5 className="modal-title" id="formModalLabel">Add Budget </h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div className='modal-body'>
+              <form action="">
+                
+                <div className="mb-3">
+                  <label htmlFor="amount" className="form-label">Amount*</label>
+                  <input type="number" name="amount" id="amount" className="form-control" required />
+                </div>
+
+              </form>
+
+            </div>
+
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="modal fade" id="addExpenseModal" tabIndex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+
+            <div className="modal-header">
+              <h5 className="modal-title" id="formModalLabel">Add Expense </h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div className='modal-body'>
+              <form action="">
+                <div className="mb-3">
+                  <label htmlFor="expenseName" className="form-label">Expense Name*</label>
+                  <input type="text" name="expenseName" id="expenseName" className="form-control" required />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="expenseDate" className="form-label">Date</label>
+                  <input type="date" name="expenseDate" id="expenseDate" className="form-control" />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="expenseCategory" className="form-label">Category*</label>
+                  <select name="expenseCategory" id="expenseCategory" className="form-select" required>
+                    <option value="">-- Select Category --</option>
+                    <option value="food">Food</option>
+                    <option value="travel">Travel</option>
+                    <option value="utilities">Utilities</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="amount" className="form-label">Amount*</label>
+                  <input type="number" name="amount" id="amount" className="form-control" required />
+                </div>
+
+              </form>
+
+            </div>
+
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+    </>
+  );
 }
--- src/Expenselist.jsx
+++ src/Expenselist.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Expenselist() {
   return (
+    <>
     <div className='listOfExpense'>
       <table>
         <tr>
@@ -15,11 +16,85 @@ export default function Expenselist() {
             <td>travel</td>
             <td>2000</td>
             <td>
-                <button>Edit</button>
-                <button>Delete</button>
+                <button data-bs-toggle="modal" data-bs-target="#editExpense">Edit</button>
+                <button data-bs-toggle="modal" data-bs-target="#deleteExpense">Delete</button>
             </td>
         </tr>
       </table>
     </div>
+
+        <div className="modal fade" id="editExpense" tabIndex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+
+            <div className="modal-header">
+              <h5 className="modal-title" id="formModalLabel">Edit Expense </h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div className='modal-body'>
+              <form action="">
+                <div className="mb-3">
+                  <label htmlFor="expenseName" className="form-label">Expense Name*</label>
+                  <input type="text" name="expenseName" id="expenseName" className="form-control" required />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="expenseDate" className="form-label">Date</label>
+                  <input type="date" name="expenseDate" id="expenseDate" className="form-control" />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="expenseCategory" className="form-label">Category*</label>
+                  <select name="expenseCategory" id="expenseCategory" className="form-select" required>
+                    <option value="">-- Select Category --</option>
+                    <option value="food">Food</option>
+                    <option value="travel">Travel</option>
+                    <option value="utilities">Utilities</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="amount" className="form-label">Amount*</label>
+                  <input type="number" name="amount" id="amount" className="form-control" required />
+                </div>
+
+              </form>
+
+            </div>
+
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="modal fade" id="deleteExpense" tabIndex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+          <div className="modal-content">
+
+
+            <div className='modal-body'>
+                <div className="mb-3">
+                  <h1>Are you sure</h1>
+
+                  <h4>You wont be able to revert this!</h4>
+                </div>
+
+            </div>
+
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="submit" className="btn btn-danger">Submit</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+    </>
   )
 }
